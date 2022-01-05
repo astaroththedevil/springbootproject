b641c4a2b81112724df9269dc84987143c1afd28
Rafal Sypniewski <r.sypniewski@moliera2.com>
2022-01-05 15:46:03 +0100

    Initial commit

---
+++ src/main/java/com/springbootproject/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.springbootproject.controller;
+
+import com.springbootproject.model.Product;
+import com.springbootproject.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product product) {
+        return service.saveProduct(product);
+    }
+
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products) {
+        return service.saveProducts(products);
+    }
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/productById/{id}")
+    public Product findProductById(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/product/{name}")
+    public Product findProductByName(@PathVariable String name) {
+        return service.getProductByName(name);
+    }
+
+    @PutMapping("/update/{id}")
+    public Product updateProduct(@RequestBody Product product, @PathVariable int id) {
+        product = service.getProductById(id);
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable int id) {
+        return service.deleteProduct(id);
+    }
+}
