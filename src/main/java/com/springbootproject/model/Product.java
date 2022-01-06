2529a88ec0584ca3bfc6d67f23a61dc6f0f94bde
rafal <astaroththedevil@gmail.com>
2022-01-06 16:13:21 +0100

    product id

--- src/main/java/com/springbootproject/model/Product.java
+++ src/main/java/com/springbootproject/model/Product.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -17,7 +14,7 @@ import javax.persistence.Table;
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int quantity;
